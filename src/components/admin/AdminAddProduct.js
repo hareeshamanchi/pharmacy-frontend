9684d0c54d6977278b9931cb3352d322535e94ad
Hareesh Amanchi <hareeshamanchi@gmail.com>
2025-07-19 23:22:24 +0530

    adprodutct

--- src/components/admin/AdminAddProduct.js
+++ src/components/admin/AdminAddProduct.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios'; // for image upload
-import api from '../utils/api'; // shared backend instance
+import api from '../../utils/api';
+ // shared backend instance
 
 import './AdminAddProduct.css';
 
